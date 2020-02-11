4563228977348d18db501f27da0eb317313fd3b0
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-02-10 18:56:19 -0800

    finished exercise 11

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,18 +11,12 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
-for arg in sys.argv:
-    print(arg)
-
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
-print("OS: ", sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
-print("Python version: ", sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
@@ -30,14 +24,8 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
-print("PID: ", os.getpid())
-
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
-print("working dir: ", os.getcwd())
-
 # Print out your machine's login name
 # YOUR CODE HERE
-
-print("login name: ", os.getlogin())
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,6 +36,16 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append(
+    {
+        "lat": -18291,
+        "lon": 2,
+        "name": "a fourth place!"
+    }
+)
+
+print(waypoints)
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
@@ -43,5 +53,13 @@ waypoints = [
 
 # YOUR CODE HERE
 
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
+
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for ele in waypoints:
+    for key in ele:
+        print(f'"{key}": {ele[key]}')
+    print()
